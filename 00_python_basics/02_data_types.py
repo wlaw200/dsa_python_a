7d5aad601dda22eadd8b8e9339c5ca5184b5efde
wlaw200 <wayne.ongwano@gmail.com>
2026-04-20 14:13:59 +0300

    up

---
+++ 00_python_basics/02_data_types.py
@@ -0,0 +1,16 @@
+name = "john"
+name1 = "john"
+name2 = "johnt"
+num = 5
+flag = True
+balance = 100.50
+
+type (name)    # <class 'str'>
+type (num)     # <class 'int'>
+type (flag)    # <class 'bool'>
+type (balance) # <class 'float'> 
+print(type(name))
+
+print(id(name))
+print(id(name1))
+print(id(name2))
---
+++ 00_python_basics/04_loops.py
@@ -0,0 +1,10 @@
+num=25
+if num > 20:
+    print(num)
+
+x=num
+y=26    
+def getValues():
+    if x > y:
+        pass
+print(x)
